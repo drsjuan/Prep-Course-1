b4743a6ebfe9ed363fa404bab89ac76acbc4998f
daniel <root@daniel-notebook>
2023-03-17 11:40:30 -0300

    init second commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -64,36 +64,39 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  if(x = y)
-    return "true"
-  }return "false"
+  if(x == y){
+    return true
+  } return false
+  } 
 
 function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-    if (length(str1) = length(str2))
-      return "true"
-}return "false"
-
+    if (str1.length = str2.length){
+         return true
+  }return false
+}    
 function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-    if(num < 90) 
-      return "true"
-    }return "false"
+    if (num < 90){
+      return true
+    } return false
+  }
 
 function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-    if (nume > 50) 
-      return "true"
-    }return "false"
+    if (num > 50){
+      return true
+    } return false
+    }
   
 function obtenerResto(x, y) {
-  // Obten el resto de la división de "x" entre "y"
+  // Obten el rest  o de la división de "x" entre "y"
   // Tu código:
   
 }
